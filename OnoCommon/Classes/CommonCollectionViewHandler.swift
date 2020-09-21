24141beafc2a773527f90305d3838418106be19a
Onur <onur.erdemol@gmail.com>
2020-09-20 22:08:32 -0700

    Some more improvements

--- OnoCommon/Classes/CommonCollectionViewHandler.swift
+++ OnoCommon/Classes/CommonCollectionViewHandler.swift
@@ -8,39 +8,23 @@ import UIKit
 
 open class CommonCollectionViewHandler: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
-    open var items: [Any] = []
-    open var controller: UIViewController!
-
-    public convenience init(items: [Any], controller: UIViewController) {
-        self.init()
-        self.items = items
-        self.controller = controller
-    }
-
     open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+        return 0
     }
 
     open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
         return UICollectionViewCell()
-
     }
 
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
-        let width: CGFloat = kScreenWidth/2
-
-        let size: CGSize = CGSize.init(width: width, height: width)
-
-        return size
+        return .zero
 
     }
 
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
 
-        return UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
+        return .zero
 
     }
-
 }
