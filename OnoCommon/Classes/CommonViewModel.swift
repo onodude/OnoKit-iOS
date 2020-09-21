cc5cf3d0ea9809aa086367a4209e1c7f57a34ea6
Onur <onur.erdemol@gmail.com>
2020-09-20 22:17:16 -0700

    Added cancellable helper

--- OnoCommon/Classes/CommonViewModel.swift
+++ OnoCommon/Classes/CommonViewModel.swift
@@ -13,14 +13,8 @@ open class CommonViewModel: ObservableObject, Identifiable {
 
     open var disposables = Set<AnyCancellable>()
 
-    open func observe(_ event: PassthroughSubject<Void, Never>, _ action: @escaping () -> Void) {
-        event.sink { (_) in action() }.store(in: &self.disposables)
-    }
-
-    open func observe(_ event: PassthroughSubject<Any, Never>, _ action: @escaping (_ something: Any) -> Void) {
-        event.sink { (something) in
-            action(something)
-        }.store(in: &self.disposables)
+    open func observe(_ cancellable: AnyCancellable) {
+        cancellable.store(in: &self.disposables)
     }
 
 }
