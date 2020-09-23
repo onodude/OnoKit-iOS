f36d8ac9f5c048f281d3349c2c409c1b582e62bf
Onur <onur.erdemol@gmail.com>
2020-09-22 18:03:11 -0700

    Added public methods

--- OnoKit/Managers/CommonApplicationManager.swift
+++ OnoKit/Managers/CommonApplicationManager.swift
@@ -5,49 +5,20 @@
 //
 
 import UIKit
-import UserNotifications
 
 public protocol CommonApplicationManagerProtocol {
+
     func start()
     func loadDeviceInfo()
+
 }
 
 open class CommonApplicationManager: NSObject, CommonApplicationManagerProtocol {
 
     open var window: UIWindow!
 
-    open func start() {
+    public func start() { }
 
-        loadDeviceInfo()
-
-        /*
-
-        ConfigManager.shared.configureLibraries()
-
-        AppDelegate.standard.window = window
-        NavigationManager.shared.window = self.window
-
-        if BackendManager.isLoggedIn() {
-            NavigationManager.shared.loggedInLaunchSequence(animated: false)
-        } else {
-            NavigationManager.shared.notLoggedInLaunchSequence(animated: false)
-        }
-
-        */
-
-    }
-
-    open func loadDeviceInfo() {
-
-        let kScreenHeight: CGFloat = UIScreen.main.bounds.height
-        let kScreenWidth: CGFloat = UIScreen.main.bounds.width
-
-        kStatusBarHeight = window.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
-        kBottomBarHeight = window.safeAreaInsets.bottom
-
-        CommonLogManager.shared.log(String(format: "screenHeight: %0.0f", kScreenHeight) )
-        CommonLogManager.shared.log(String(format: "screenWidth: %0.0f", kScreenWidth) )
-
-    }
+    public func loadDeviceInfo() { }
 
 }
