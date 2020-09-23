efc7ea7485f052db9adb12ea0ff1a25b196b1f9b
Onur <onur.erdemol@gmail.com>
2020-09-22 21:26:17 -0700

    Edited some extensions

---
+++ OnoKit/Extensions/UIFontExtension.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Onur Erdemol
+//  Copyright © 2020 Onur Erdemol
+//  All rights reserved
+//
+
+import UIKit
+
+public extension UIFont {
+
+    convenience init?(_ name: String, _ size: CGFloat) {
+
+        self.init(name: name, size: size)
+
+    }
+
+}
